8100195fa660e48c105d843147f5f03721f7121e
mike connor <mike@rocketnumber9.org>
2017-09-01 13:06:53 -0500

    iframe test, icon style
    crawls DOM and test iframes for VPAID.  Places adchoices icon.

--- js/VPAIDAdChoices.js
+++ js/VPAIDAdChoices.js
@@ -1,35 +1,47 @@
 var VPAIDAdChoices = function() {
   var VPAIDCreative;
   var adIFrame = document.getElementsByTagName("iframe")[0];
-  var fn = adIFrame.contentWindow['getVPAIDAd'];
 
-  if (fn && typeof fn == 'function') {
-    VPAIDCreative = fn();
-    console.log('VPAIDCreative +++');
-    console.log(VPAIDCreative);
-    console.log("_adWidth " + VPAIDCreative._adWidth);
-    console.log('getAdWidth ' + VPAIDCreative.getAdWidth());
+  // var _iframes = document.getElementsByTagName("iframe");
 
-    console.log("_adHeight " + VPAIDCreative._adHeight);
-    console.log('getAdHeight ' + VPAIDCreative.getAdHeight());
+  var _iframes = Array.from(document.getElementsByTagName("iframe"));
+  //console.log(_iframes);
+  _iframes.map((adIFrame) => {
+    var fn = adIFrame.contentWindow['getVPAIDAd'];
 
-    var x = adIFrame.contentWindow;
-	console.log(x);
-    // if (x.document) {
-    //   x = adIFrame.document;
-    //   // to ensure compability
-    // }
+    if (fn && typeof fn == 'function') {
+      VPAIDCreative = fn();
+      var _contentWindow = adIFrame.contentWindow;
+      var iconW = 70;
 
-    var mydiv = x.document.createElement("div");
-    x.document.body.appendChild(mydiv);
-    var myIMG = x.document.createElement("img");
-    myIMG.setAttribute("src", "http://c.betrad.com/geo/c_70.png");
-    myIMG.setAttribute("width", "70");
-    myIMG.setAttribute("height", "20");
-    //myIMG.setAttribute("onClick","window.location='https://www.google.com'");
-    mydiv.appendChild(myIMG);
+      var mydiv = _contentWindow
+        .document
+        .createElement("div");
+      var xpos = VPAIDCreative.getAdWidth() - iconW - 6;
 
-  }
+      _contentWindow
+        .document
+        .body
+        .appendChild(mydiv);
+      var myIMG = _contentWindow
+        .document
+        .createElement("img");
+      var atag = _contentWindow
+        .document
+        .createElement("a");
+
+      atag.setAttribute("href", "http://info.evidon.com/more_info/18126");
+      atag.setAttribute("target", "_blank");
+
+      myIMG.setAttribute("src", "http://c.betrad.com/geo/c_70.png");
+      mydiv.appendChild(atag);
+      atag.appendChild(myIMG);
+      mydiv.setAttribute("style", `position: absolute; left: ${xpos}px; z-index:10003; margin-top:2px`);
+
+    } else {
+      console.log('NON VPAID IFRAME');
+    }
+  });
 
 };
 window.setTimeout(VPAIDAdChoices, 3000);
