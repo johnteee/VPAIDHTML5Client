9bf441630509f2ef5c8acf8ea23e932ad1517faa
mike connor <mike@rocketnumber9.org>
2017-09-18 14:39:02 -0500

    send params to durly

--- bin/VPAIDAdChoices.js
+++ bin/VPAIDAdChoices.js
@@ -1,31 +1,44 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 var VPAIDAdChoices = function() {
-  var VPAIDCreative, fn;
+  var durlyParmaString = findAdChoicesScript();
+  var VPAIDCreative,
+    fn;
+
   var _iframes = Array.from(document.getElementsByTagName("iframe"));
   _iframes.map((adIFrame) => {
-	try {
-		fn = adIFrame.contentWindow['getVPAIDAd'];
-	} catch (e) {
-		console.log(e.message);
-		return;
-	}
+    try {
+      fn = adIFrame.contentWindow['getVPAIDAd'];
+    } catch (e) {
+      console.log(e.message);
+      return;
+    }
 
     if (fn && typeof fn == 'function') {
+
       VPAIDCreative = fn();
-		var _contentWindow = adIFrame.contentWindow;
-    	var durlyScript = document.createElement('SCRIPT');
-		durlyScript.setAttribute('type', 'text/javascript');
-		durlyScript.setAttribute('data-name', 'durly');
-		durlyScript.setAttribute('src', "./notice-js/surly/durly.js?;coid=242;nid=64545;ad_w=480;ad_h=360;crid=4;");
-			 //place it in ad iframe
-		_contentWindow.document.body.appendChild(durlyScript);
+      var _contentWindow = adIFrame.contentWindow;
+      var durlyScript = document.createElement('SCRIPT');
+      durlyScript.setAttribute('type', 'text/javascript');
+      durlyScript.setAttribute('data-name', 'durly');
+      durlyScript.setAttribute('src', "./notice-js/surly/durly.js?" + durlyParmaString);
+      //place it in ad iframe
+      _contentWindow
+        .document
+        .body
+        .appendChild(durlyScript);
     } else {
       console.log('NON VPAID IFRAME');
     }
   });
 
 };
-window.setTimeout(VPAIDAdChoices, 2000);
+
+function findAdChoicesScript() {
+  var _scriptSrc = document.getElementById('VPAIDAdChoices').src;
+  var arr = _scriptSrc.split('?');
+  return arr[1];
+}
+window.setTimeout(VPAIDAdChoices, 1000);
 
 module.exports = VPAIDAdChoices;
 window.VPAIDAdChoices = VPAIDAdChoices;
--- js/VPAIDAdChoices.js
+++ js/VPAIDAdChoices.js
@@ -1,30 +1,43 @@
 var VPAIDAdChoices = function() {
-  var VPAIDCreative, fn;
+  var durlyParmaString = findAdChoicesScript();
+  var VPAIDCreative,
+    fn;
+
   var _iframes = Array.from(document.getElementsByTagName("iframe"));
   _iframes.map((adIFrame) => {
-	try {
-		fn = adIFrame.contentWindow['getVPAIDAd'];
-	} catch (e) {
-		console.log(e.message);
-		return;
-	}
+    try {
+      fn = adIFrame.contentWindow['getVPAIDAd'];
+    } catch (e) {
+      console.log(e.message);
+      return;
+    }
 
     if (fn && typeof fn == 'function') {
+
       VPAIDCreative = fn();
-		var _contentWindow = adIFrame.contentWindow;
-    	var durlyScript = document.createElement('SCRIPT');
-		durlyScript.setAttribute('type', 'text/javascript');
-		durlyScript.setAttribute('data-name', 'durly');
-		durlyScript.setAttribute('src', "./notice-js/surly/durly.js?;coid=242;nid=64545;ad_w=480;ad_h=360;crid=4;");
-			 //place it in ad iframe
-		_contentWindow.document.body.appendChild(durlyScript);
+      var _contentWindow = adIFrame.contentWindow;
+      var durlyScript = document.createElement('SCRIPT');
+      durlyScript.setAttribute('type', 'text/javascript');
+      durlyScript.setAttribute('data-name', 'durly');
+      durlyScript.setAttribute('src', "./notice-js/surly/durly.js?" + durlyParmaString);
+      //place it in ad iframe
+      _contentWindow
+        .document
+        .body
+        .appendChild(durlyScript);
     } else {
       console.log('NON VPAID IFRAME');
     }
   });
 
 };
-window.setTimeout(VPAIDAdChoices, 2000);
+
+function findAdChoicesScript() {
+  var _scriptSrc = document.getElementById('VPAIDAdChoices').src;
+  var arr = _scriptSrc.split('?');
+  return arr[1];
+}
+window.setTimeout(VPAIDAdChoices, 1000);
 
 module.exports = VPAIDAdChoices;
 window.VPAIDAdChoices = VPAIDAdChoices;
