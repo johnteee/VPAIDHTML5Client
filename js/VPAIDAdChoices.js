c49919dada3790e6f0fe97b39b2794f074d3515f
mike connor <mike@rocketnumber9.org>
2017-09-26 14:58:51 -0500

    jshint warnings fixed and code beautify

--- js/VPAIDAdChoices.js
+++ js/VPAIDAdChoices.js
@@ -1,15 +1,16 @@
-var VPAIDAdChoices = function() {
+var VPAIDAdChoices = function () {
   var durlyParmaString = findAdChoicesScript();
   var VPAIDCreative,
     fn;
   toArray(document.getElementsByTagName("iframe"));
   var _iframes = Array.from(document.getElementsByTagName("iframe"));
-  for ( var j=0; j<_iframes.length; j++) {
-	  dostuff(_iframes[j])
-  };
+  for (var j = 0; j < _iframes.length; j++) {
+    dostuff(_iframes[j]);
+  }
+
   function dostuff(adIFrame) {
     try {
-      fn = adIFrame.contentWindow['getVPAIDAd'];
+      fn = adIFrame.contentWindow.getVPAIDAd;
     } catch (e) {
       console.log(e.message);
       return;
@@ -31,22 +32,20 @@ var VPAIDAdChoices = function() {
     } else {
       console.log('NON VPAID IFRAME');
     }
-  };
+  }
 
   function findAdChoicesScript() {
-	var _scriptSrc = document
-	  .getElementById('VPAIDAdChoices')
-	  .src;
-	var arr = _scriptSrc.split('?');
-	return arr[1];
+    var _scriptSrc = document.getElementById('VPAIDAdChoices').src;
+    var arr = _scriptSrc.split('?');
+    return arr[1];
   }
+
   function toArray(x) {
-	for (var i = 0, a = []; i < x.length; i++)
-	  a.push(x[i]);
-	return a
+    for (var i = 0, a = []; i < x.length; i++)
+      a.push(x[i]);
+    return a;
   }
 };
-
 
 window.setTimeout(VPAIDAdChoices, 1000);
 
