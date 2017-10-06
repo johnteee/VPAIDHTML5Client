7a3d064dd4cfbba04f6712fd73dd2d9c48c7384f
mike connor <mike@rocketnumber9.org>
2017-10-06 11:55:05 -0500

    delay_start param
    delay_start param normally delays the start of ba.js. But here we delay the start of loading durly.js  and give the VPAID  a chance to fire up.

--- js/VPAIDAdChoices.js
+++ js/VPAIDAdChoices.js
@@ -1,10 +1,15 @@
 var VPAIDAdChoices = function () {
   var durlyParmaString = getParamsFromMyScript("VPAIDAdChoices");
 
-  var arr = durlyParmaString.split(";");
+  var paramArr = durlyParmaString.split(";");
+
+  
   
   var dstart = 0;
-  dstart = parseInt(pullValue("delay_start", arr, "="));
+  dstart = parseInt(pullValue("delay_start", paramArr, "="));
+  // strip off delay param... we will delay durly rather than ba.  Give VPAID a chance to fire up
+  paramArr = removeParam("delay_start", paramArr);
+
   var _iframes = toArray(document.getElementsByTagName("iframe"));
  
   for (var j = 0; j < _iframes.length; j++) {
@@ -34,7 +39,14 @@ var VPAIDAdChoices = function () {
     }
   }
 
-
+  function removeParam(key, arr) {
+    for (var i = 0; i < arr.length; i++) {
+      if (arr[i].indexOf(key) !== -1) {
+        var arr2 = arr.splice(i, 1);
+        return arr2;
+      }
+    }
+  } 
   function pullValue(key, arr, delim) {
     for (var i = 0; i < arr.length; i++) {
       if (arr[i].indexOf(key) !== -1) {
