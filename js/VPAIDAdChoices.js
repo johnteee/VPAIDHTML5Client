44b6f4a30cb98f84a03b6c04522ebff85523d889
mike connor <mike@rocketnumber9.org>
2017-10-05 19:53:55 -0500

    delay_start worked in
    plus other optimizations.
    TODO … don’t pass delay into ba

--- js/VPAIDAdChoices.js
+++ js/VPAIDAdChoices.js
@@ -2,45 +2,54 @@ var VPAIDAdChoices = function () {
   console.log("VPAIDAdChoices");
 
   var durlyParmaString = getParamsFromMyScript('VPAIDAdChoices');
-  var VPAIDCreative,
-    fn;
+
+  var arr = durlyParmaString.split(';');
+  
+  var dstart = 0;
+  dstart = parseInt(pullValue("delay_start", arr, "="));
+
+
+  var VPAIDCreative;
   var _iframes = toArray(document.getElementsByTagName("iframe"));
+
+ 
   for (var j = 0; j < _iframes.length; j++) {
-    dostuff(_iframes[j]);
+    //dostuff(_iframes[j]);
+    setTimeout(dostuff, dstart*1000, _iframes[j]);
   }
   function dostuff(adIFrame) {
-    try {
-      fn = adIFrame.contentWindow.getVPAIDAd;
-    } catch (e) {
-      console.log(e.message);
-      return;
-    }
-
-    if (fn && typeof fn === 'function') {
+    if (adIFrame.contentWindow.getVPAIDAd && typeof adIFrame.contentWindow.getVPAIDAd === "function") {
+      console.log("we have VPAID");
+      //fn = adIFrame.contentWindow.getVPAIDAd;
       // if  VPAID in iframe, inject durly.js
-      VPAIDCreative = fn();
+      VPAIDCreative = adIFrame.contentWindow.getVPAIDAd();
       var _contentWindow = adIFrame.contentWindow;
-      var durlyScript = document.createElement('SCRIPT');
-      durlyScript.setAttribute('type', 'text/javascript');
-      durlyScript.setAttribute('data-name', 'durly');
+      var durlyScript = document.createElement("SCRIPT");
+      durlyScript.setAttribute("type", "text/javascript");
+      durlyScript.setAttribute("data-name", "durly");
       if (adIFrame.clientWidth) {
-        durlyParmaString = durlyParmaString.concat(';ad_w=' + adIFrame.clientWidth);
+        durlyParmaString = durlyParmaString.concat(";ad_w=" + adIFrame.clientWidth);
       }
       if (adIFrame.clientHeight) {
-        durlyParmaString = durlyParmaString.concat(';ad_h=' + adIFrame.clientHeight);
+        durlyParmaString = durlyParmaString.concat(";ad_h=" + adIFrame.clientHeight);
       }
-     //durlyScript.setAttribute('src', "./notice-js/surly/durly.js?" + durlyParmaString);
-      durlyScript.setAttribute('src', "https://c.evidon.com/durly.js?" + durlyParmaString);
+      //durlyScript.setAttribute('src', "./notice-js/surly/durly.js?" + durlyParmaString);
+      durlyScript.setAttribute("src", "https://c.evidon.com/durly.js?" + durlyParmaString);
       //place it in ad iframe
-      _contentWindow
-        .document
-        .body
-        .appendChild(durlyScript);
+      _contentWindow.document.body.appendChild(durlyScript);
     } else {
-      console.log('NON VPAID IFRAME');
+      console.log("NON VPAID IFRAME");
     }
   }
 
+
+  function pullValue(key, arr, delim) {
+   for (var i = 0; i < arr.length; i++) {
+     if (arr[i].indexOf(key) !== -1) {
+       return arr[i].split(delim)[1];
+     }
+   }
+  }
   function getParamsFromMyScript(idname) {
     var _scriptSrc = document.getElementById(idname).src;
     var arr = _scriptSrc.split('?');
@@ -48,12 +57,12 @@ var VPAIDAdChoices = function () {
   }
 
   function toArray(x) {
-    for (var i = 0, a = []; i < x.length; i++)
+    for (var i = 0, a = []; i < x.length; i++) {
       a.push(x[i]);
+    }
     return a;
   }
 };
 
-window.setTimeout(VPAIDAdChoices, 2000);
 module.exports = VPAIDAdChoices;
 window.VPAIDAdChoices = VPAIDAdChoices;
