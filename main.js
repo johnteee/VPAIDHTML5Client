ce50b7da41d96730125a71d21cb7e3d4fc053a52
mike connor <mike@rocketnumber9.org>
2018-03-29 17:45:24 -0500

    Update 2018-03-29T22:45:20.733Z

--- main.js
+++ main.js
@@ -38,7 +38,7 @@
   
   function getRandomAd() {
     //return urls[Math.round( (urls.length - 1) * Math.random())];
-    return urls[0];
+    return urls[1];
   }
   
   var currentAd = getRandomAd();
